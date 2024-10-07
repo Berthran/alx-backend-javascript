612c6c286ac9a8a859068b35d5c3e429f888b479
Daniel Berthran <berthlearn@gmail.com>
2024-10-07 03:18:25 +0100

    Handlers to a promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,7 @@
-
+/* eslint-disable no-unused-vars */
+export default function handleResponseFromAPI(promise) {
+  promise.then((value) => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  }).catch((err) => new Error());
+}
