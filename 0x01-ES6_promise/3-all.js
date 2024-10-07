858bdc9d7747a659a3a338705739ffcd15d8a185
Daniel Berthran <berthlearn@gmail.com>
2024-10-07 04:23:34 +0100

    add catch handler

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,10 +5,11 @@ export default function handleProfileSignup() {
   const photoPromise = uploadPhoto();
   const userPromise = createUser();
 
-  Promise.all([photoPromise, userPromise])
+  return Promise.all([photoPromise, userPromise])
     .then((values) => {
       console.log(values[0].body,
         values[1].firstName,
         values[1].lastName);
-    });
+    })
+    .catch(() => console.log('Signup sysyem offline'))
 }
