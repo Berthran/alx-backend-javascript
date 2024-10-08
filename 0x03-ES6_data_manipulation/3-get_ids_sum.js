22c993bd1e96a2d55cd32b9d7f841c8886ff9a3d
Daniel Berthran <berthlearn@gmail.com>
2024-10-08 04:35:29 +0100

    eslint fix V

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentsIdsSum(students) {
-	return students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
+  return students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
 }
