c411815b72e2eac574497ad1372194448f7892fb
Daniel Berthran <berthlearn@gmail.com>
2024-10-09 19:15:39 +0100

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Task 1
 interface Teacher {
 	readonly firstName: string;		// Modifiable only during execution
 	readonly lastName: string;
@@ -7,12 +8,53 @@ interface Teacher {
 	[key: string]: any;				// Allow any additional attributes
 }
 
-// const teacher3: Teacher = {
-// 	firstName: 'John',
-// 	fullTimeEmployee: false,
-// 	lastName: 'Doe',
-// 	location: 'London',
-// 	contract: false,
-//   };
-  
-// console.log(teacher3);
+// Task 2
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+// Task 3
+interface printTeacherFunction {
+	(
+		firstName: string,		// Argument 1
+		lastName: string,		// Argument 2
+	)
+	: string;					// Return type
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+	return `${firstName[0]}. ${lastName}`;
+};
+
+// Task 4
+interface Constructor {
+	firstName: string;
+	lastName: string;
+}
+
+interface Student {
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+class StudentClass implements Student {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return "Currently working";
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
+
+const student = new StudentClass("John", "Doe");
+console.log(student.workOnHomework()); // Output: Currently working
+console.log(student.displayName());     // Output: John
