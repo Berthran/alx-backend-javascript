19465a7fd759429efc5135de994ac6b74b49ec2f
Daniel Berthran <berthlearn@gmail.com>
2024-10-09 14:43:42 +0100

    instanceof Set + startString != undefined + ele != undefined

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,16 +2,18 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set) || set.size === 0) {
     return ('');
   }
-  if (startString === '' || !(startString instanceof String) || startString === undefined) {
+  if (startString === '' || startString === undefined) {
     return ('');
   }
 
   let result = '';
 
   for (const ele of set) {
-    if (ele.startsWith(startString)) {
-      const cleanEle = ele.slice(startString.length);
-      result += result ? `-${cleanEle}` : cleanEle;
+    if (ele !== undefined) {
+      if (ele.startsWith(startString)) {
+        const cleanEle = ele.slice(startString.length);
+        result += result ? `-${cleanEle}` : cleanEle;
+      }
     }
   }
 
