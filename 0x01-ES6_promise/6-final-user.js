a3e273808a3997fdff9476ff32bdd416df64266f
Daniel Berthran <berthlearn@gmail.com>
2024-10-12 02:48:28 +0100

    suggested solution

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,23 +1,41 @@
+// import signUpUser from './4-user-promise';
+// import uploadPhoto from './5-photo-reject';
+
+// // #1 Using promises
+// // export default function handleProfileSignUp(firstName, lastName, fileName) {
+// //   const userDetails = signUpUser(firstName, lastName);
+// //   const photoUpload = uploadPhoto(fileName);
+// //   return Promise.allSettled([userDetails, photoUpload])
+// //     .then((results) => results);
+// // }
+
+// // #2 Using async/await
+// export default async function handleProfileSignUp(firstName, lastName, fileName) {
+//   const userDetails = signUpUser(firstName, lastName);
+//   const photoUpload = uploadPhoto(fileName);
+//   const results = await Promise.allSettled([userDetails, photoUpload]);
+//   return results.forEach((result) => [
+//     {
+//       status: result.status,
+//       value: result.value || result.reason,
+//     },
+//   ]);
+// }
+
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-// #1 Using promises
-// export default function handleProfileSignUp(firstName, lastName, fileName) {
-//   const userDetails = signUpUser(firstName, lastName);
-//   const photoUpload = uploadPhoto(fileName);
-//   return Promise.allSettled([userDetails, photoUpload])
-//     .then((results) => results);
-// }
-
-// #2 Using async/await
-export default async function handleProfileSignUp(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  // Call the two functions that return promises
   const userDetails = signUpUser(firstName, lastName);
   const photoUpload = uploadPhoto(fileName);
-  const results = await Promise.allSettled([userDetails, photoUpload]);
-  return results.forEach((result) => [
-    {
-      status: result.status,
-      value: result.value || result.reason,
-    },
-  ]);
+
+  // Use Promise.allSettled to wait for both promises to settle
+  return Promise.allSettled([userDetails, photoUpload])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason };
+    }));
 }
