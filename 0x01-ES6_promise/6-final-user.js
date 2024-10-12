73fa8b25b4958ac1ab0e8f0026182d784d2a2d11
Daniel Berthran <berthlearn@gmail.com>
2024-10-12 02:15:48 +0100

    Promise.allSettled -> settle all promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,18 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
+// #1 Using promises
+// export default function handleProfileSignUp(firstName, lastName, fileName) {
+//   const userDetails = signUpUser(firstName, lastName);
+//   const photoUpload = uploadPhoto(fileName);
+//   return Promise.allSettled([userDetails, photoUpload])
+//     .then((results) => results);
+// }
+
+// #2 Using async/await
+export default async function handleProfileSignUp(firstName, lastName, fileName) {
+  const userDetails = signUpUser(firstName, lastName);
+  const photoUpload = uploadPhoto(fileName);
+  const results = await Promise.allSettled([userDetails, photoUpload]);
+  return results;
+}
