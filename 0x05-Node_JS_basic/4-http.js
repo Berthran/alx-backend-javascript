aff0bd4a8b074503cb96edb3198bd672adde8dee
Daniel Berthran <berthlearn@gmail.com>
2025-01-27 19:15:17 +0100

    create simple HTTP server on Node

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' }); // Set response content type to plain text
+  res.end('Hello ALX!'); // Send the response
+});
+
+// Server listens on port 1245
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+// Export the app
+module.exports = app;
